d3adc47d83c1ed2f35f4387b1624e45dadbc4ee6
shauryatawakley <75786498+shauryatawakley@users.noreply.github.com>
2021-02-23 17:56:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var a,b
+function setup() {
+  createCanvas(800,400);
+  a=createSprite(400, 200, 50, 50);
+  b=createSprite(200,100,40,40);
+  a.shapeColor="green"
+  b.shapeColor='green'
+}
+
+function draw() {
+  background(255,255,255);
+  a.x=mouseX
+  a.y=mouseY
+if(a.x-b.x<a.width/2+b.width/2 && b.x-a.x<a.width/2+b.width/2 && a.y-b.y<a.width/2+b.width/2 && b.y-a.y<a.width/2+b.width/2){
+  a.shapeColor="red"
+  b.shapeColor='red'
+}
+else{
+  a.shapeColor="green"
+  b.shapeColor='green'
+}
+  drawSprites();
+}
